b71b86eee3ebebec977c840d9f593411f1962823
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2018-10-15 15:31:21 +0200

    .

---
+++ game.js
@@ -0,0 +1,80 @@
+var vraag1 = prompt("hoeveel is 1+1?")
+	if(vraag1 == "2"){
+		alert("goedzo!")
+
+		var vraag2 = prompt("hoeveel is 2x2?")
+			if(vraag2 == "4"){
+				alert("goedzo!")
+
+				var vraag3 = prompt("hoeveel is 4x5?")
+					if(vraag3 == "20"){
+						alert("goedzo!")
+
+						var vraag4 = prompt("hoeveel is 70-69?")
+							if(vraag4 == "1"){
+								alert("goedzo!")
+
+								var vraag5 = prompt("hoeveel is 100:5?")
+									if(vraag5 == "20"){
+										alert("goedzo!")
+
+										var vraag6 = prompt("wat is (33x3)+1?")
+											if(vraag6 == "100"){
+												alert("goedzo!")
+
+												var vraag7 = prompt("wat is 89-45?")
+													if(vraag7 == "44"){
+														alert("goedzo!")
+
+														var vraag8 = prompt("wat is 10x34?")
+															if(vraag8 == "340"){
+																alert("goedzo!")
+
+																var vraag9 = prompt("wat is 99:3?")
+																	if(vraag9 == "3"){
+																		alert("goedzo!")
+
+																		alert("en nu de bonusvraag",)
+																		var vraag10 = prompt("hoeveel is 110:10x9:3?","je mag deze vraag fout hebben :)")
+																			if(vraag10 == "3"){
+																				alert("je hebt alle vragen goed, gefeliciteerd!")
+																			}else{
+																				alert("helaas, je hebt de bonusvraag fout")
+																				alert("maar gelukkig mag je deze fout hebben, je hebt de game succesvol voltooid :)")
+																			}
+																	}else{
+																		alert("jammer joh")
+																		lert("game over")
+																	}
+															}else{
+																alert("jammer joh")
+																lert("game over")
+															}
+													}else{
+														alert("jammer joh")
+														lert("game over")	
+													}
+											}else{
+												alert("jammer joh")
+												lert("game over")
+											}
+									}else{
+										alert("jammer joh")
+										alert("game over")
+									}
+							}else{
+								alert("jammer joh")
+								alert("game over")
+							}
+					}else{
+						alert("jammer joh")
+						alert("game over")
+					}
+			}else{
+				alert("jammer joh")
+				alert("game over")
+			}
+	}else{
+		alert("jammer joh")
+		alert("game over")
+	}
